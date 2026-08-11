578d43cfa1b7bd32323658bd61afb5fc6a9c985b
b1rdaaj <anna.acap@gmail.com>
2026-08-10 22:47:50 -0300

    atualizando projeto

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,19 @@
-// 6359c740822ebc7e243e4bc6f27e018c
+const container = document.querySelector('.container');
+const search = document.querySelector('.search-box button');
+const weatherBox = document.querySelector('.weather-box');
+const weatherDetails = document.querySelector('.weather-details');
+const error404 = document.querySelector('.not-found');
+
+search.addEventListener('click', () =>{
+    const APIKey = '6359c740822ebc7e243e4bc6f27e018c';
+    const city = document.querySelector('search-box input').ariaValueMax;
+
+    if (city === ' ')
+        return;
+
+    fetch ('https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}').then(response => response.json()).then(json => {
+        if(json.cod === '404'){
+            container.style.height = '400px';
+        }
+    })
+})
